00373938971b63aa0c57010ecdd0436a16e07115
spanishgum <aps10d@my.fsu.edu>
2016-10-24 01:41:51 -0400

    caching

--- aeSIFT.py
+++ aeSIFT.py
@@ -32,9 +32,10 @@ import matplotlib.pyplot as plt
 
 saved_cmp = {}
 bestCount = {}
-debugging = False
+debugging = True
+sift = cv2.xfeatures2d.SIFT_create()
 def mycomparer(a, b, qc, qc2=None):
-    
+    global sift
     i2, i1 = a[0], b[0]
     im2, im1 = a[1], b[1]
     
@@ -42,17 +43,28 @@ def mycomparer(a, b, qc, qc2=None):
     assert 'xfeatures2d' in dir(cv2), 'required opencv tools ' \
         'missing: xfeatures2d'
     
-    # cache check
-    sift = kp1 = ds1 = None
+    # cache check and load if possible
+    kp1 = ds1 = kp2 = ds2 = None
     if qc in saved_cmp:
         if i1 in saved_cmp[qc]:
-            sift, kp1, ds1 = saved_cmp[qc][i1]
-    if sift is None:
-        sift = cv2.xfeatures2d.SIFT_create()
+            kp1, ds1 = saved_cmp[qc][i1]
+    else:
+        saved_cmp[qc] = {}
+    if qc2 in saved_cmp:
+        if i2 in saved_cmp[qc2]:
+            kp2, ds2 = saved_cmp[qc2][i2]
+    else:
+        saved_cmp[qc2] = {}
+        
+    # load and fill cache if needed
+    if kp1 is None:
         kp1, ds1 = sift.detectAndCompute(im1, None)
+        saved_cmp[qc][i1] = kp1, ds1
+    if kp2 is None:
+        kp2, ds2 = sift.detectAndCompute(im2, None)
+        saved_cmp[qc2][i2] = kp2, ds2
     
-    kp2, ds2 = sift.detectAndCompute(im2, None)
-    
+    # matching
     flann = cv2.FlannBasedMatcher({'algorithm' : 0, 'trees' : 5},{'checks':50})
     matches = flann.knnMatch(ds1,ds2,k=2)
     
@@ -62,7 +74,7 @@ def mycomparer(a, b, qc, qc2=None):
     # ratio test as per Lowe's paper
     matchCount = 0
     for i,(m,n) in enumerate(matches):
-        if m.distance < 0.7 * n.distance:
+        if m.distance < 0.75 * n.distance:
             matchCount += 1
             matchesMask[i] = [1,0]
     
@@ -76,7 +88,7 @@ def mycomparer(a, b, qc, qc2=None):
         
         img3 = cv2.drawMatchesKnn(im1,kp1,im2,kp2,matches,None,**draw_params)
         plt.imshow(img3)
-        plt.savefig('sometest.png')
+        plt.savefig('sometest.jpg')
         
         print('i1:', i1, 'c1:', qc)
         print('i2:', i2, 'c2:', qc2)
@@ -100,7 +112,7 @@ def runtest(d):
     t = time.time()
     
     manage = Manager(d, __name__, cmp=mycomparer)
-    manage.alltests(N=100)
+    manage.alltests(N=2)
     # manage.alltests(qcs2plot=['c5'], N=100)
     
     print(time.time() - t, 'sec')
