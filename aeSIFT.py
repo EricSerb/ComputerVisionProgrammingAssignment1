7fb0e244d40068f68881684e50a71875ae67ef5b
spanishgum <aps10d@my.fsu.edu>
2016-10-24 00:30:03 -0400

    miner txt fixes

--- aeSIFT.py
+++ aeSIFT.py
@@ -61,24 +61,25 @@ def mycomparer2(a, b, qc, qc2=None):
     assert 'xfeatures2d' in dir(cv2), 'required opencv tools ' \
         'missing: xfeatures2d'
         
-        sift = cv2.xfeatures2d.SIFT_create()
-        kp1, ds1 = sift.detectAndCompute(im1, None)
-        kp2, ds2 = sift.detectAndCompute(im2, None)
-        
-        flann = cv2.FlannBasedMatcher({'algorithm' : 0, 'trees' : 5},{'checks':50})
-        matches = flann.knnMatch(ds1,ds2,k=2)
-        
-        # Need to draw only good matches, so create a mask
-        matchesMask = [[0,0] for i in range(len(matches))]
-        
-        # ratio test as per Lowe's paper
-        matchCount = 0
-        for i,(m,n) in enumerate(matches):
-            if m.distance < 0.7 * n.distance:
-                matchCount += 1
-                matchesMask[i] = [1,0]
-        
-        
+    sift = cv2.xfeatures2d.SIFT_create()
+    kp1, ds1 = sift.detectAndCompute(im1, None)
+    kp2, ds2 = sift.detectAndCompute(im2, None)
+    
+    flann = cv2.FlannBasedMatcher({'algorithm' : 0, 'trees' : 5},{'checks':50})
+    matches = flann.knnMatch(ds1,ds2,k=2)
+    
+    # Need to draw only good matches, so create a mask
+    matchesMask = [[0,0] for i in range(len(matches))]
+    
+    # ratio test as per Lowe's paper
+    matchCount = 0
+    for i,(m,n) in enumerate(matches):
+        if m.distance < 0.7 * n.distance:
+            matchCount += 1
+            matchesMask[i] = [1,0]
+    
+    # test drawing for debugging
+    if True:
         draw_params = dict(
             matchColor = (0,255,0),
             singlePointColor = (255,0,0),
@@ -88,10 +89,6 @@ def mycomparer2(a, b, qc, qc2=None):
         img3 = cv2.drawMatchesKnn(im1,kp1,im2,kp2,matches,None,**draw_params)
         plt.imshow(img3)
         plt.savefig('sometest.png')
-        
-    else:
-        print('Warning: your platform does not have ' \
-            'required support for this module')
     
     # print(bestCount)
     # bestCount.setdefault(qc, matchCount)
